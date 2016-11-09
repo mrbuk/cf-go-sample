0c08e99f36909450d027a4ade1dd367c1986fb32
Markus Bukowski <me@mrbuk.de>
2016-11-09 12:31:01 +0000

    Simple web service in go. Returns details about request for GET / request

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	filteredRequest := map[string]interface{}{
+		"method":     r.Method,
+		"url":        r.URL.Path,
+		"proto":      r.Proto,
+		"header":     r.Header,
+		"host":       r.Host,
+		"remoteAddr": r.RemoteAddr,
+		"requestURI": r.RequestURI,
+	}
+
+	b, err := json.MarshalIndent(filteredRequest, "", "  ")
+	if err != nil {
+		log.Print("Error:", err, "occured")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Sorry a problem occured")
+
+	} else {
+		s := string(b)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, s)
+	}
+}
+
+func main() {
+
+	var port string
+
+	if configuredPort := os.Getenv("PORT"); configuredPort != "" {
+		port = configuredPort
+	} else {
+		port = "8080"
+	}
+
+	log.Print("Binding service to: ", port)
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
